f2cba6073c4718b4eba9c190a99f56479a02e466
J-Roque <johel0rmr@gmail.com>
2025-05-03 15:04:54 -0500

    feat:modificamos puerto por defecto

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 require('dotenv').config();
 
 module.exports = {
-  host:     process.env.DB_HOST     || 'localhost',
-  port:     process.env.DB_PORT     || '3306',
+  host:     process.env.DB_HOST     || '',
+  port:     process.env.DB_PORT     || '',
   database: process.env.DB_NAME     || '',
   user:     process.env.DB_USER     || '',
   password: process.env.DB_PASSWORD || '',
