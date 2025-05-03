f2369a87b3fbad44da752fca973ca83ce150fd68
J-Roque <johel0rmr@gmail.com>
2025-05-03 15:28:57 -0500

    feat: correccion de query table name productos

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -28,7 +28,7 @@ const formatResults = rows =>
 const getProducts = async (req, res) => {
   try {
     const [rows] = await pool.query(
-      'SELECT nombre_prod, precio, thema_imagen, collecion, tipo_prod FROM product'
+      'SELECT nombre_prod, precio, thema_imagen, collecion, tipo_prod FROM productos'
     );
     res.json(formatResults(rows));
   } catch (err) {
@@ -42,7 +42,7 @@ const getProductLenguage = async (req, res) => {
     const [rows] = await pool.query(
       `SELECT a.id_prod, a.nombre_prod, a.precio, a.cod_coleccion, a.thema_imagen, a.collecion,
               b.arte, a.tipo_prod, b.color, b.talla, b.descripcion
-       FROM product a
+       FROM productos a
        INNER JOIN producto_detalle b ON a.id_prod = b.id_prod
        WHERE a.collecion = ?`,
       [lenguage]
@@ -60,7 +60,7 @@ const getProduct = async (req, res) => {
     const [rows] = await pool.query(
       `SELECT a.id_prod, a.nombre_prod, a.precio, a.cod_coleccion, a.thema_imagen, a.collecion,
               b.arte, a.tipo_prod, b.color, b.talla, b.descripcion
-       FROM product a
+       FROM productos a
        INNER JOIN producto_detalle b ON a.id_prod = b.id_prod
        WHERE a.collecion = ? AND a.tipo_prod = ? LIMIT 1`,
       [coleccion, tipo_prod]
@@ -86,7 +86,7 @@ const getcollectionProd = async (req, res) => {
   try {
     const { prod } = req.params;
     const [rows] = await pool.query(
-      'SELECT precio, thema_imagen, collecion, nombre_prod FROM product WHERE collecion = ?',
+      'SELECT precio, thema_imagen, collecion, nombre_prod FROM productos WHERE collecion = ?',
       [prod]
     );
     res.json(formatResults(rows));
@@ -102,7 +102,7 @@ const getcollectionProdInfo = async (req, res) => {
     const [rows] = await pool.query(
       `SELECT a.id_prod, a.nombre_prod, a.precio, a.cod_coleccion, a.thema_imagen, a.collecion,
               b.arte, a.tipo_prod, b.color, b.talla, b.descripcion
-       FROM product a
+       FROM productos a
        INNER JOIN producto_detalle b ON a.id_prod = b.id_prod
        WHERE a.collecion = ? AND a.nombre_prod = ?`,
       [prod, info]
